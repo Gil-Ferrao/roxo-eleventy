5c932e642d681a6ae566bc5c2556cebd6c37fb75
Gil-Ferrao <g.ferrao@campus.fct.unl.pt>
2023-08-07 18:08:37 +0100

    a

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
   return {
+    markdownTemplateEngine: false,
+    htmlTemplateEngine: false,
     passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
     templateFormats: ["html", "njk", "md"],
